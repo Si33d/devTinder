a81f5857e69be7c2e493b560d324a5f4c272c236
sohamnagare17 <nagaresoham7@gmail.com>
2025-01-27 03:57:08 +0530

    exploring get post api

--- src/app.js
+++ src/app.js
@@ -3,8 +3,26 @@ const express=require('express');
 //create a express js application .This is an instance of express 
 const app=express();
 
+//This will only handle GET call to /user
+app.get("/user",(req,res)=>
+{
+    res.send({firstName:"Siddharth",lastName:"Kumbhar"})
+})
+
+app.post("/user",(req,res)=>
+{
+    //save data to DB
+   // console.log("Save Data to Database")
+    res.send("Data Successfully saved to the Database")
+})
+
+app.delete("/user",(req,res)=>
+{
+    res.send("Deleted Successfully  ")
+})
 
 //handle the request
+//TGis will match all the Http method API calls to /test
 app.use("/test",(req,res)=>
 {
     res.send("Hello from the Server")
